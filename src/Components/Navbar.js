8a87b9ca044c6ec434a782ed5a8f42401aa98c00
Rushil Patel <ra8patel@uwaterloo.ca>
2020-09-11 13:06:25 -0400

    adding navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import React from 'react';
+
+
+const Navbar = () => {
+
+    return(
+        <div>
+            <StyledNavBar>
+                <NavLink>About</NavLink>
+                <NavLink>Experience</NavLink>
+                <NavLink>Projects</NavLink>
+                <NavLink>Extra</NavLink>
+            </StyledNavBar>
+        </div>
+    );
+}
+
+const StyledNavBar = styled.div`
+    padding: 10px;
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 0px;
+    background-color: cyan;
+    position:fixed;
+    width: 100%;
+`;
+
+const NavLink = styled.div`
+    margin-right: 10px;
+    margin-left: 10px;
+    font-size: 30px;
+`;
+
+export default Navbar;
