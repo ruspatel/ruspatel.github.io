02a56a00d49f074eac60b2ce4e09913aca58a588
Rushil Patel <ra8patel@uwaterloo.ca>
2020-09-14 21:08:54 -0400

    Updates

---
+++ precache-manifest.ddbaccbfaa7d492eeb1e37cc61d39016.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f405743c7721506956db507512d91ea",
+    "url": "/ruspatel.github.io/index.html"
+  },
+  {
+    "revision": "30f50ad088ceb04aee85",
+    "url": "/ruspatel.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c0e8038c2277120218f",
+    "url": "/ruspatel.github.io/static/js/2.f7097862.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ruspatel.github.io/static/js/2.f7097862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f50ad088ceb04aee85",
+    "url": "/ruspatel.github.io/static/js/main.40a90f3a.chunk.js"
+  },
+  {
+    "revision": "e201c686b34d86556472",
+    "url": "/ruspatel.github.io/static/js/runtime-main.6d24a601.js"
+  },
+  {
+    "revision": "9a1f011d0e05011007f7fc801d2e5fcf",
+    "url": "/ruspatel.github.io/static/media/androidstudio.9a1f011d.png"
+  },
+  {
+    "revision": "adb6bf1903ce85eb0ea7a53cea5ad842",
+    "url": "/ruspatel.github.io/static/media/economical.adb6bf19.jpeg"
+  },
+  {
+    "revision": "2fc258b13c206135301505d9a396871a",
+    "url": "/ruspatel.github.io/static/media/reactlogo.2fc258b1.png"
+  }
+]);
