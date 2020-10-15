fcfad15329e9ebf54c605fe4772aa043a6812f04
Rushil Patel <ra8patel@uwaterloo.ca>
2020-10-15 10:43:25 -0400

    md file appearing on web page

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Element} from 'react-scroll';
 import {Header} from './CommonComponents';
 import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import readMe from './README.md';
 
 
 
 const Notes = () => {
+    const [file, setFile] = useState({file: null});
+    useEffect(() => {
+        fetch(readMe).then((response) => response.text()).then((text) => {
+            setFile({file: text})
+        }) 
+    })
+
     return(
         <div>
         <NotesBox>
@@ -13,6 +22,7 @@ const Notes = () => {
                     <Header style={{"margin-top": "75px"}}>Notes</Header>
             </Element>
                 <h1>Coming Soon ...</h1>
+                <ReactMarkdown source={file.file} />
         </NotesBox>
 
         </div>
